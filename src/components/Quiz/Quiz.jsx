e5dbeb6f81b45818d9ec33638b8b5d9749ce26f3
Elijah <xrealntrue16x@gmail.com>
2025-03-19 21:48:43 -0400

    Answers showing up

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -9,6 +9,7 @@ const Quiz = () => {
   const [visible, setVisible] = useState(false);
   const [songs, setSongs] = useState([]);
   const [Question, setQuestion] = useState(1);
+  const [answer, setAnswer] = useState("");
 
   const { isStarted, setIsStarted } = useContext(quizContext);
 
@@ -18,6 +19,17 @@ const Quiz = () => {
     console.log("Reset");
     console.log(e);
     setIsStarted(false);
+  };
+
+  const handleSubmitQuiz = (e) => {
+    e.preventDefault();
+    console.log(answer);
+  };
+
+  const handleAnswerSelect = (e) => {
+    const selection = e.target.value;
+    console.log(selection);
+    setAnswer(selection);
   };
 
   //fade in animation
@@ -36,7 +48,12 @@ const Quiz = () => {
     getSavedSongs().then((songs) => {
       if (songs) {
         setSongs(songs);
+        console.log(
+          "Songs",
+          songs.map((song) => song)
+        );
         console.log(songs.map((song) => song.name));
+        console.log(songs.map((song) => song.artist));
       } else {
         console.error("No Songs Found");
       }
@@ -44,16 +61,24 @@ const Quiz = () => {
   }, []);
 
   return (
-    <form className={`quiz ${visible ? "quiz__visible" : ""}`}>
+    <form
+      className={`quiz ${visible ? "quiz__visible" : ""}`}
+      onSubmit={handleSubmitQuiz}
+    >
       <h1 className="quiz__header">Question {Question}</h1>
 
       {songs.length > 0 ? (
         <ul className="quiz__options">
           {songs.slice(0, 4).map((song) => (
-            <li key={song?.track?.id}>
+            <li key={song?.id}>
               <label>
-                <input type="radio" name="quizOption" value={song?.track?.id} />
-                {song.name}
+                <input
+                  type="radio"
+                  name="quiz__option"
+                  value={song?.name}
+                  onChange={handleAnswerSelect}
+                />
+                {song.name} - {song.artist}
               </label>
             </li>
           ))}
@@ -61,6 +86,10 @@ const Quiz = () => {
       ) : (
         <p className="quiz__options">Loading Songs...</p>
       )}
+
+      <button className="quiz__submit-btn" type="submit">
+        Submit
+      </button>
 
       <button className="quiz__reset-btn" onClick={handleResetQuiz}>
         Reset
