eed0c5f5d14ff41af7a5cb5d81720ca54415e8b5
Elijah <xrealntrue16x@gmail.com>
2025-05-29 13:56:46 -0400

    Hash ROuter Added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
